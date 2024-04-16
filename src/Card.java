61254b04159103bca584fd70e147a51374b422af
Eason Lu <aelnosu@outlook.com>
2024-04-16 09:26:46 -0700

    [Unstable] Initial Commit
    Add the given class file to the project.

---
+++ src/Card.java
@@ -0,0 +1,58 @@
+public class Card implements Comparable<Card> {
+    private final String mySymbol;
+    private final int myRank;
+    private final char mySuit;
+
+    public Card(char rank, char suit) {
+        // Note how switch works.  It's really useful.
+        // Note also the character arithmetic.
+        //
+        // Remember, a char has a Unicode
+        // value and that needs to be converted.
+        switch (rank) {
+            case 'A':
+                myRank = 14;
+                break;
+            case 'K':
+                myRank = 13;
+                break;
+            case 'Q':
+                myRank = 12;
+                break;
+            case 'J':
+                myRank = 11;
+                break;
+            case 'T':
+                myRank = 10;
+                break;
+            default:
+                myRank = rank - '0';
+        }
+        mySuit = suit;
+        mySymbol = "" + rank + suit;
+    }
+
+    public int getRank() {
+        return myRank;
+    }
+
+    public char getSuit() {
+        return mySuit;
+    }
+
+    public String getSymbol() {
+        return mySymbol;
+    }
+
+    public int compareTo(Card card) {
+        // YOU WRITE THIS
+    }
+
+    public boolean equals(Card card) {
+        // YOU WRITE THIS
+    }
+
+    public String toString() {
+        return mySymbol;
+    }
+}
